04d58b8101e2985012832b1c077adf8501ddee99
Marcus Rocha <sif.marcusvinicius@gmail.com>
2023-02-16 11:00:13 -0300

    Creating HttpGet with params and querystrings

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -10,14 +11,47 @@ namespace SmartSchool.WebAPI.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
-        public AlunoController()
-        {
-            
-        }
-
+        public List<Aluno> Alunos = new List<Aluno>(){
+            new Aluno(){
+                Id = 1,
+                Nome = "Marcus",
+                Sobrenome = "Rocha",
+                Telefone = "123123123"
+            },
+            new Aluno(){
+                Id = 2,
+                Nome = "Vinicius",
+                Sobrenome = "Alves",
+                Telefone = "321321321"
+            },
+            new Aluno(){
+                Id = 3,
+                Nome = "Rosana",
+                Sobrenome = "Maria",
+                Telefone = "321321321"
+            }
+        };
+       
+        public AlunoController() {}
+        
+        [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Alunos: Marcus, Vinicius");
+            return Ok(Alunos);
+        }
+
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            return aluno == null ? BadRequest("Aluno não encontrado") : Ok(aluno);
+        }
+
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+            return aluno == null ? BadRequest("Aluno não encontrado") : Ok(aluno);
         }
     }
 }
